6949de468fea5fdd7ba6afd354bbfc167cfc4717
Yanchen <yz6956@nyu.edu>
2023-02-28 20:44:23 -0500

    upload sampleAPI file

---
+++ sampleAPI.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+from waitress import serve
+import logging
+logger = logging.getLogger('waitress')
+logger.setLevel(logging.INFO)
+
+@app.route('get', methods=['GET'])
+def testGet():
+    return jsonify('message', 'test works')
+
+@app.route('post', methods=['POST'])
+def testPost():
+    data = request.json
+    print(data)
+    return 'POST works'
+
+@app.route('/')
+def is_up():
+    return 'Server is up'
+
+if __name__ == '__main__':
+    serve(app, host='0,0,0,0', port=8080)
